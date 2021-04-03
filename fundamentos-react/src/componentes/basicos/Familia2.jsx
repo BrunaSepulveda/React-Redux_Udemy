5e57d79cbb757d193e09d798ffffb18f2f3d05c8
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-04-03 18:01:49 -0300

    adiciona no comentario uma opção de refatoração da função que renderiza os filhos

--- fundamentos-react/src/componentes/basicos/Familia2.jsx
+++ fundamentos-react/src/componentes/basicos/Familia2.jsx
@@ -4,9 +4,15 @@ export default props => {
   return(
     <div>
       {
-        React.Children.map(props.children, (children) => { 
-          return cloneElement(children,props)
+        React.Children.map(props.children, (children, i) => { 
+          return cloneElement(children,{ ... props, key: i })
         })
+        /*
+        outra forma de fazer a função sem chamar o React diretamente
+        props.children.map((children, i) => {
+          return cloneElement(children,{ ... props, key: i })
+        })
+        */
       }
     </div>
   );
